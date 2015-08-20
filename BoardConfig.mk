93241d1d7fa897a464e7a9e3f73fcf7e9d51bced
hiroshi <sakura.bar.com@gmail.com>
2015-08-20 19:54:46 +0900

    Change Variant_Config

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,7 +19,7 @@ TARGET_OTA_ASSERT_DEVICE := klte,kltedcmactive,sc02g,SC02G,SC-02G
 
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/samsung/klteactive
-TARGET_KERNEL_VARIANT_CONFIG := msm8974pro_sec_klte_eur_defconfig
+TARGET_KERNEL_VARIANT_CONFIG := msm8974pro_sec_kactivelte_dcm_defconfig
 
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_msm
